cb30acface2827932b354474373a3146673499d9
Harry Akbar Ali Munir <harry.akbar@ntucenterprise.sg>
2023-05-27 16:28:57 +0800

    Setup Profile page

--- app/page.js
+++ app/page.js
@@ -1,95 +1,80 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import "./global.css";
+
+const Container = (props) => {
+  return (
+    <div
+      className={[
+        "flex p-2 mb-4 row drop-shadow-md bg-[#FFFDF6] rounded-md w-full place-content-between",
+        props.margin || "p-2",
+      ]}
+    >
+      {props.children}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Container>
+        <img
+          className="w-6 h-6"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/768px-Google_Chrome_icon_%28February_2022%29.svg.png"
+          alt="icon"
         />
-      </div>
+        <div className="flex row">
+          <span>Dashboard</span>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+            alt="profile"
+            className="w-6 h-6"
+          />
+        </div>
+      </Container>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Container>
+        <div className="bg-white drop-shadow-md m-10 rounded-md p-4 ">
+          <h3 className="font-bold">Create Your Job Profile</h3>
+          <div className="flex row items-center">
+            <span>LinkedIn Profile</span>
+            <button className="ml-2 text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+              Fill Profile with LinkedIn
+            </button>
+          </div>
+          <div className="flex flex-col">
+            <span>Name</span>
+            <input type="text" className="border border-slate-300 rounded-md" />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <span>Email</span>
+            <input
+              type="email"
+              className="border border-slate-300 rounded-md"
+            />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+            <span>Phone Number</span>
+            <input
+              type="phone"
+              className="border border-slate-300 rounded-md mb-2"
+            />
+            <div className="border"></div>
+            <div>Experiences</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <span>Company Name</span>
+            <input type="text" className="border border-slate-300 rounded-md" />
+            <span>Role</span>
+            <input type="text" className="border border-slate-300 rounded-md" />
+            <span>Start Date</span>
+            <input type="date" className="border border-slate-300 rounded-md" />
+
+            <span>End Date</span>
+            <input type="date" className="border border-slate-300 rounded-md" />
+            <span>Description</span>
+            <textarea className="border border-slate-300 rounded-md" />
+          </div>
+        </div>
+      </Container>
     </main>
-  )
+  );
 }
