215303a4cacc6db68e05f04fcb74cc870683ede8
Harry Akbar Ali Munir <harry.akbar@ntucenterprise.sg>
2023-05-29 22:31:08 +0800

    Fix styles, implement others Improve with Magic feature

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import styles from "./page.module.css";
 import "./global.css";
 import Form from "./components/form";
+import classNames from "classnames";
 
 const Container = (props) => {
   return (
     <div
-      className={[
-        "flex p-2 mb-4 row drop-shadow-md bg-[#FFFDF6] rounded-md w-full place-content-between",
-        props.margin || "p-2",
-      ]}
+      className={classNames(
+        "flex flex-row mb-4 row drop-shadow-md bg-[#FFFDF6] rounded-md w-full place-content-between justify-between",
+        props.margin || "m-2",
+        props.padding || "p-2"
+      )}
     >
       {props.children}
     </div>
@@ -17,19 +19,19 @@ const Container = (props) => {
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Container>
+    <main className={classNames(styles.main, "p-4 sm:p-8")}>
+      <Container margin="m-0">
         <img
           className="w-6 h-6"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/768px-Google_Chrome_icon_%28February_2022%29.svg.png"
           alt="icon"
         />
         <div className="flex row">
-          <span>Dashboard</span>
+          <span className="font-bold">Dashboard</span>
           <img
             src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
             alt="profile"
-            className="w-6 h-6"
+            className="w-6 h-6 ml-2"
           />
         </div>
       </Container>
