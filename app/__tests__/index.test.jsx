5ea90d4a2bef3c0e771b0391db1055e0eb325741
Harry Akbar Ali Munir <harryakbaram@gmail.com>
2023-06-06 14:49:11 +0800

    Fix

--- app/__tests__/index.test.jsx
+++ app/__tests__/index.test.jsx
@@ -2,6 +2,25 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "../page";
 
+// Mock the environment variables
+jest.mock("next/config", () => () => ({
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_SUPABASE_URL: "mocked-supabase-url",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "mocked-supabase-anon-key",
+  },
+}));
+
+jest.mock("@supabase/supabase-js", () => ({
+  createClient: jest.fn(() => ({
+    // mock implementation of the Supabase client methods
+    // you can customize these mock methods based on your test requirements
+    from: jest.fn().mockReturnThis(),
+    select: jest.fn().mockReturnThis(),
+    eq: jest.fn().mockReturnThis(),
+    // ...add more mock methods as needed
+  })),
+}));
+
 describe("Home", () => {
   it("renders a Dashboard", () => {
     render(<Home />);
