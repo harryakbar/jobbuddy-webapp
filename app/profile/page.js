d10bf88d77171c686ab48889f059101cb5e620f6
Harry Akbar Ali Munir <harryakbaram@gmail.com>
2023-06-17 15:59:02 +0800

    Implement initial sign in page and landing page (#5)
    * Implement initial sign in page and landing page
    
    * Fix build
    
    * Fix unit test

---
+++ app/profile/page.js
@@ -0,0 +1,62 @@
+"use client";
+import styles from "../page.module.css";
+import "../global.css";
+import Form from "../components/form";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabasePublicKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabasePublicKey);
+
+const Container = (props) => {
+  return (
+    <div
+      className={classNames(
+        "flex flex-row mb-4 row drop-shadow-md bg-[#FFFDF6] rounded-md w-full place-content-between justify-between",
+        props.margin || "m-2",
+        props.padding || "p-2",
+        props.className || ""
+      )}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const response = supabase.auth?.onAuthStateChange((_, session) => {
+      if (session) {
+        setUser(session.user);
+      }
+    });
+    return () => {
+      if (typeof response?.data?.unsubscribe === "function") {
+        response?.data?.unsubscribe();
+      }
+    };
+  }, []);
+
+  return (
+    <main className={classNames(styles.main, "p-4 sm:p-8")}>
+      <Container margin="m-0">
+        <img
+          className="w-6 h-6"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/768px-Google_Chrome_icon_%28February_2022%29.svg.png"
+          alt="icon"
+        />
+        <div className="flex row">
+          <span className="font-bold">Dashboard</span>
+        </div>
+      </Container>
+
+      <Container className="min-h-screen">
+        {user ? <Form user={user} /> : "Loading..."}
+      </Container>
+    </main>
+  );
+}
